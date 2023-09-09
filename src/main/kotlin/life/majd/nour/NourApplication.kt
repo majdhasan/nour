376ae2482667156415c18458b959194d864e1058
majdhasan <majd.hasan@hotmail.com>
2023-09-09 12:58:16 +0200

    initial commit

---
+++ src/main/kotlin/life/majd/nour/NourApplication.kt
@@ -0,0 +1,11 @@
+package life.majd.nour
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NourApplication
+
+fun main(args: Array<String>) {
+    runApplication<NourApplication>(*args)
+}
---
+++ src/test/kotlin/life/majd/nour/NourApplicationTests.kt
@@ -0,0 +1,13 @@
+package life.majd.nour
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NourApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
